28bf98e4ad9f279bdda3b5478eb2895897ffd85e
SavinienD <savinien.dreyfuss@epitech.eu>
2026-02-02 14:53:29 +0100

    udpate

--- terraform/envs/onprem/main.tf
+++ terraform/envs/onprem/main.tf
@@ -1,6 +1,6 @@
 provider "pfsense" {
-  url      = "https://ns3050272.ip-51-255-76.eu:8006/"
-  username = "GR38"
-  password = "7ZC7rm7k"
-  tls_insecure = true  # certificat auto-signé
+  url          = "https://ns3050272.ip-51-255-76.eu:8006/"
+  username     = "GR38"
+  password     = "7ZC7rm7k"
+  tls_insecure = true # certificat auto-signé
 }
