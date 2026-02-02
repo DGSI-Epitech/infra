35efc61f8dd196c222c922b258538fe6c1e92794
SavinienD <savinien.dreyfuss@epitech.eu>
2026-02-02 14:16:46 +0100

    RBEI/pfsense + acces depuis terra

---
+++ terraform/envs/remote/main.tf
@@ -0,0 +1,6 @@
+provider "pfsense" {
+  url      = "https://ns3183326.ip-146-59-253.eu:8006/" 
+  username = "GR38"
+  password = "7ZC7rm7k" 
+  tls_insecure = true  # ertificat auto-signé
+}
--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,1 +1,8 @@
-
+terraform {
+  required_providers {
+    pfsense = {
+      source  = "RBEI/pfsense"
+      version = "~> 0.2" # Prend la dernière version compatible 0.2.x
+    }
+  }
+}
