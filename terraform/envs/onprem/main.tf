11d96f7d77c3c15094c3c91270c626fd74d44ef3
melizzzz <152108637+melizzzz@users.noreply.github.com>
2026-04-25 23:22:29 +0200

    suppression de ubuntu template dans la liste des modules terraform car elle essaie de recreer une autre template ce qui est déjà fait par packer

--- terraform/envs/onprem/main.tf
+++ terraform/envs/onprem/main.tf
@@ -1,18 +1,9 @@
-module "ubuntu_template" {
-  source = "../../modules/ubuntu-template"
-
-  proxmox_node = var.proxmox_node
-  vm_id        = var.template_ubuntu_vm_id
-  storage_iso  = var.storage_iso
-  storage_vm   = var.storage_vm
-}
-
 module "services_vm" {
   source = "../../modules/services-vm"
 
   proxmox_node      = var.proxmox_node
   vm_id             = var.services_vm_id
-  template_vm_id    = module.ubuntu_template.vm_id
+  template_vm_id    = var.template_ubuntu_vm_id
   vm_ip_cidr        = var.vm_ip_cidr
   vm_gateway        = var.vm_gateway
   vm_ssh_public_key = var.vm_ssh_public_key
@@ -23,9 +14,9 @@ module "services_vm" {
 
 module "vault_vm" {
   source = "../../modules/vault-vm"
-
+  depends_on = [module.services_vm]
   proxmox_node      = var.proxmox_node
-  template_vm_id    = module.ubuntu_template.vm_id
+  template_vm_id    = var.template_ubuntu_vm_id
   vm_id             = var.vault_vm_id
   vm_ip_cidr        = var.vault_vm_ip_cidr
   vm_gateway        = var.vm_gateway
