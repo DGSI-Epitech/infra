01cc50058a1bf4c2cfe0393dfd69597654f3dc4e
Ryan WITTERT <ryan.wittert@outlook.fr>
2026-05-21 18:06:02 +0200

    minor changes

--- terraform/envs/onprem/main.tf
+++ terraform/envs/onprem/main.tf
@@ -1,26 +1,38 @@
 module "services_vm" {
-  source = "../../modules/services-vm"
+  source = "../../modules/vm-proxmox"
 
   proxmox_node      = var.proxmox_node
+  name              = "services-vm"
   vm_id             = var.services_vm_id
   template_vm_id    = var.template_ubuntu_vm_id
   vm_ssh_public_key = var.vm_ssh_public_key
   storage_iso       = var.storage_iso
   storage_vm        = var.storage_vm
-  depends_on = [module.ops_vm]      #attendre que Proxmox déverrouille le template
+  vm_cores          = 2
+  vm_memory_mb      = 4096
+  disk_size_gb      = 20
+  network_bridge    = var.vm_network_bridge
+  network_tag       = var.vm_network_vlan_tag
+  tags              = ["services", "ubuntu-22.04"]
+  depends_on        = [module.ops_vm] # attendre que Proxmox déverrouille le template
 }
 
 module "ops_vm" {
-  source = "../../modules/ops-vm"
+  source = "../../modules/vm-proxmox"
+
   proxmox_node      = var.proxmox_node
-  template_vm_id    = var.template_ubuntu_vm_id
+  name              = "ops-vm"
   vm_id             = var.ops_vm_id
+  template_vm_id    = var.template_ubuntu_vm_id
   vm_ssh_public_key = var.vm_ssh_public_key
   storage_iso       = var.storage_iso
   storage_vm        = var.storage_vm
   vm_cores          = 4
   vm_memory_mb      = 8192
   disk_size_gb      = 30
+  network_bridge    = var.vm_network_bridge
+  network_tag       = var.vm_network_vlan_tag
+  tags              = ["ops", "ubuntu-22.04"]
 }
 
 module "pfsense" {
@@ -31,5 +43,7 @@ module "pfsense" {
   template_vm_id = var.pfsense_template_id
   lan_bridge     = "vmbr1"
   wan_bridge     = "vmbr2"
+  lan_vlan_tag   = var.lan_vlan_tag
+  wan_vlan_tag   = var.wan_vlan_tag
   vm_name        = "pfsense-fw-01"
 }
--- terraform/envs/remote/main.tf
+++ terraform/envs/remote/main.tf
@@ -1,6 +1,48 @@
-provider "pfsense" {
-  url      = "https://ns3183326.ip-146-59-253.eu:8006/"
-  username = "GR38"
-  password = "7ZC7rm7k"
-  tls_insecure = true  # ertificat auto-signé
+module "remote_services_vm" {
+  source = "../../modules/vm-proxmox"
+
+  proxmox_node      = var.proxmox_node
+  name              = "remote-services-vm"
+  vm_id             = var.services_vm_id
+  template_vm_id    = var.template_ubuntu_vm_id
+  vm_ssh_public_key = local.cloud_init_ssh_key
+  storage_iso       = var.storage_iso
+  storage_vm        = var.storage_vm
+  vm_cores          = 2
+  vm_memory_mb      = 4096
+  disk_size_gb      = 20
+  network_bridge    = var.vm_network_bridge
+  network_tag       = var.vm_network_vlan_tag
+  tags              = ["services", "ubuntu-22.04"]
+}
+
+module "remote_ops_vm" {
+  source = "../../modules/vm-proxmox"
+
+  proxmox_node      = var.proxmox_node
+  name              = "remote-ops-vm"
+  vm_id             = var.ops_vm_id
+  template_vm_id    = var.template_ubuntu_vm_id
+  vm_ssh_public_key = local.cloud_init_ssh_key
+  storage_iso       = var.storage_iso
+  storage_vm        = var.storage_vm
+  vm_cores          = 4
+  vm_memory_mb      = 8192
+  disk_size_gb      = 30
+  network_bridge    = var.vm_network_bridge
+  network_tag       = var.vm_network_vlan_tag
+  tags              = ["ops", "ubuntu-22.04"]
+}
+
+module "remote_pfsense" {
+  source = "../../modules/pfsense"
+
+  proxmox_node   = var.proxmox_node
+  vm_id          = var.pfsense_vm_id
+  template_vm_id = var.template_pfsense_vm_id
+  vm_name        = "remote-pfsense-fw"
+  lan_bridge     = var.lan_bridge
+  wan_bridge     = var.wan_bridge
+  lan_vlan_tag   = var.lan_vlan_tag
+  wan_vlan_tag   = var.wan_vlan_tag
 }
